935e85ac076d2427669e65dc3c5baa8471145544
lukailun <957458041@qq.com>
2022-06-02 18:13:31 +0800

    bloc

--- lib/presentation/bloc/game_bloc.dart
+++ lib/presentation/bloc/game_bloc.dart
@@ -1,48 +1,70 @@
-/// Copyright (c) 2022 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to
-/// deal in the Software without restriction, including without limitation the
-/// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-/// sell copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge,
-/// publish, distribute, sublicense, create a derivative work, and/or sell
-/// copies of the Software in any work that is designed, intended, or marketed
-/// for pedagogical or instructional purposes related to programming, coding,
-/// application development, or information technology.  Permission for such
-/// use, copying, modification, merger, publication, distribution, sublicensing,
-///  creation of derivative works, or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-///  IN THE SOFTWARE.
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 
 import '../../data.dart';
 import '../../domain.dart';
 
 part 'game_event.dart';
+
 part 'game_state.dart';
 
 /// Handles all logic related to the game.
 class GameBloc extends Bloc<GameEvent, GameState> {
   /// Constructor
-  GameBloc(this._statsRepository) : super(GameState(guesses: emptyGuesses()));
+  GameBloc(this._statsRepository)
+      : super(GameState(
+          guesses: emptyGuesses(),
+        )) {
+    on<GameStarted>(_onGameStarted);
+    on<LetterKeyPressed>(_onLetterKeyPressed, transformer: sequential());
+    on<GameFinished>(_onGameFinished);
+  }
 
   /// Interacts with storage for updating game stats.
   final GameStatsRepository _statsRepository;
 
-  // TODO: Add logic for GameStarted
-  // TODO: Add logic for GameFinished
-  // TODO: Add logic for LetterKeyPressed
+  void _onGameStarted(
+    GameStarted event,
+    Emitter<GameState> emit,
+  ) {
+    print('Game has started!');
+    final puzzle = nextPuzzle(puzzles);
+    final guesses = emptyGuesses();
+    emit(GameState(
+      guesses: guesses,
+      puzzle: puzzle,
+    ));
+  }
+
+  Future<void> _onLetterKeyPressed(
+    LetterKeyPressed event,
+    Emitter<GameState> emit,
+  ) async {
+    final puzzle = state.puzzle;
+    final guesses = addLetterToGuesses(state.guesses, event.letter);
+    emit(state.copyWith(
+      guesses: guesses,
+    ));
+    final words = guesses
+        .map((guess) => guess.join())
+        .where((word) => word.isNotEmpty)
+        .toList();
+    final hasWon = words.contains(puzzle);
+    final hasMaxAttempts = words.length == kMaxGuesses &&
+        words.every((word) => word.length == kWordLength);
+    if (hasWon || hasMaxAttempts) {
+      add(GameFinished(hasWon: hasWon));
+    }
+  }
+
+  Future<void> _onGameFinished(
+    GameFinished event,
+    Emitter<GameState> emit,
+  ) async {
+    await _statsRepository.addGameFinished(hasWon: event.hasWon);
+    emit(state.copyWith(
+      status: event.hasWon ? GameStatus.success : GameStatus.failure,
+    ));
+  }
 }
